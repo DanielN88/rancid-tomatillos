9b64c7251ad94179de5bc90f88cfc0a49ca73fe9
Dylan Duke <laytonmaes@gmail.com>
2022-05-12 13:35:16 -0600

    Create file for sample data, add basic movie data and complex movie data.

---
+++ src/data.js
@@ -0,0 +1,88 @@
+const movies = [
+    {
+        "id": 694919,
+        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        "title": "Money Plane",
+        "average_rating": 6.875,
+        "release_date": "2020-09-29"
+        },
+        {
+        "id": 337401,
+        "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+        "title": "Mulan",
+        "average_rating": 5.1,
+        "release_date": "2020-09-04"
+        },
+        {
+        "id": 718444,
+        "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
+        "title": "Rogue",
+        "average_rating": 7.333333333333333,
+        "release_date": "2020-08-20"
+        },
+        {
+            "id": 539885,
+            "poster_path": "https://image.tmdb.org/t/p/original//qzA87Wf4jo1h8JMk9GilyIYvwsA.jpg",
+            "backdrop_path": "https://image.tmdb.org/t/p/original//54yOImQgj8i85u9hxxnaIQBRUuo.jpg",
+            "title": "Ava",
+            "average_rating": 5.875,
+            "release_date": "2020-07-02"
+            },
+]
+
+const movieDetails = [
+    {
+        "id": 694919,
+        "title": "Money Plane",
+        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        "release_date": "2020-09-29",
+        "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
+        "genres": [
+        "Action"
+        ],
+        "budget": 0,
+        "revenue": 0,
+        "runtime": 82,
+        "tagline": "",
+        "average_rating": 6.875
+    },
+    {
+        "id": 337401,
+        "title": "Mulan",
+        "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+        "release_date": "2020-09-04",
+        "overview": "When the Emperor of China issues a decree that one man per family must serve in the Imperial Chinese Army to defend the country from Huns, Hua Mulan, the eldest daughter of an honored warrior, steps in to take the place of her ailing father. She is spirited, determined and quick on her feet. Disguised as a man by the name of Hua Jun, she is tested every step of the way and must harness her innermost strength and embrace her true potential.",
+        "genres": [
+        "Action",
+        "Adventure",
+        "Drama",
+        "Fantasy"
+        ],
+        "budget": 200000000,
+        "revenue": 57000000,
+        "runtime": 115,
+        "tagline": "",
+        "average_rating": 5.1
+    },
+    {
+        "id": 718444,
+        "title": "Rogue",
+        "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
+        "release_date": "2020-08-20",
+        "overview": "Battle-hardened O’Hara leads a lively mercenary team of soldiers on a daring mission: rescue hostages from their captors in remote Africa. But as the mission goes awry and the team is stranded, O’Hara’s squad must face a bloody, brutal encounter with a gang of rebels.",
+        "genres": [
+        "Action"
+        ],
+        "budget": 0,
+        "revenue": 0,
+        "runtime": 106,
+        "tagline": "When the hunter becomes the prey.",
+        "average_rating": 7.333333333333333
+    }
+]
