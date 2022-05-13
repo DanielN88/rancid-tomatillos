bc3d63abf9332f79eb7d130b841a79d95fa282d3
Daniel Neer <daniel.r.neer@gmail.com>
2022-05-12 18:29:46 -0700

    Display all movies

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './MovieCard.css';
 
-const MovieCard = () => {
+const MovieCard = (props) => {
     return (
         <div className="card">
-            <h2>Title</h2>
+            <h2>{props.title}</h2>
             <div className="image">
-            <img src="https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg" />
+            <img src={props.imgUrl} />
             </div>
-        <h3>Rating: 5.1</h3>
-        <h4>Release Date : 2020-09-04</h4>
+        <h3>Rating: {props.rating}</h3>
+        <h4>Release Date: {props.releaseDate}</h4>
         </div>
     )
 }
