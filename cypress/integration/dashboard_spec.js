ee8be1414453e6b8b4c5fb713e3157000fcc5dab
Daniel Neer <daniel.r.neer@gmail.com>
2022-05-17 14:50:52 -0700

    Set up cypress testing

---
+++ cypress/integration/dashboard_spec.js
@@ -0,0 +1,34 @@
+// dashboard_spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Dashboard Flow', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('displays a title of Rancid Tomatillos', () => {
+    cy.get('h1').first().should('have.text', 'Rancid Tomatillos')
+  })
+
+  it('displays a list of cards with a length of 40', () => {
+    cy.get('.card').should('have.length', 40)
+  })
+
+  it('displays a title for each movie card', () => {
+    cy.get('.card h2').first().should('have.text', 'Money Plane')
+    cy.get('.card h2').eq(1).should('have.text', 'Mulan')
+    cy.get('.card h2').eq(2).should('have.text', 'Rogue')
+  })
+
+  it('Anchor tag has an expected link', () => {
+    
+  })
+
+  // it('displays an image for a movie card', () => {
+  //   cy.get('.card h2').first().should('have.text', 'Money Plane')
+  // })
+})
