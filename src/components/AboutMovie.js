54e699a6acaca6ef54b35c84477eda8fef481931
Daniel Neer <daniel.r.neer@gmail.com>
2022-05-23 14:17:50 -0700

    Refactor

--- src/components/AboutMovie.js
+++ src/components/AboutMovie.js
@@ -1,70 +1,91 @@
-import React, { Component }from 'react';
-
+import React, { Component } from "react";
 
 class AboutMovie extends Component {
-    constructor () {
-        super ()
-        this.state = {favoriteMovies: [], isFavorite: false, isError: false, errorMessage: ""}
-    }
+  constructor() {
+    super();
+    this.state = {
+      favoriteMovies: [],
+      isFavorite: false,
+      isError: false,
+      errorMessage: "",
+    };
+  }
 
-   componentDidMount = () => {
-    fetch('http://localhost:3001/api/v1/user/favorites')
-    .then((response) => {
-        if(response.ok) {
-        return response.json()
+  componentDidMount = () => {
+    fetch("http://localhost:3001/api/v1/user/favorites")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
         } else {
-            this.setState({favoriteMovies: [], isFavorite: false, isError: true, errorMessage: response.statusText})
-        throw Error(response.statusText)
+          this.setState({
+            favoriteMovies: [],
+            isFavorite: false,
+            isError: true,
+            errorMessage: response.statusText,
+          });
+          throw Error(response.statusText);
         }
-    })
-    .then((response) => {
-        this.setState({favoriteMovies: response.favorites, isFavorite: false, isError: false, errorMessage: ""})
-    })
-    .then(() => {
-        let favMovie = this.state.favoriteMovies.find(movie => {
-            return movie.id === parseInt(this.props.movieId)
-        })
-        if(favMovie){
-            this.setState((prevState) =>
-            {return {
-                ...prevState, isFavorite: true
-            }
-        })
+      })
+      .then((response) => {
+        this.setState({
+          favoriteMovies: response.favorites,
+          isFavorite: false,
+          isError: false,
+          errorMessage: "",
+        });
+      })
+      .then(() => {
+        let favMovie = this.state.favoriteMovies.find((movie) => {
+          return movie.id === parseInt(this.props.movieId);
+        });
+        if (favMovie) {
+          this.setState((prevState) => {
+            return {
+              ...prevState,
+              isFavorite: true,
+            };
+          });
         }
-    })
-    .catch((err) => {
-        console.log(err)
-        })
-   }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-   changeState = () => {
-    this.setState(prevState => {
-        return{...prevState, isFavorite: true}
-    })
-   }
+  changeState = () => {
+    this.setState((prevState) => {
+      return { ...prevState, isFavorite: true };
+    });
+  };
 
-   render() {
-       return (
-           <div className='about-movie'>
-               <div className='title-favorite'>
-               <h2>{this.props.movie.title}</h2>
-               {this.state.isFavorite && 
-               <button className='favorite-button' disabled>In Your Favorites!</button>
-               }
-               {!this.state.isFavorite && 
-               <button className='favorite-button' onClick={() => {
-                   this.props.addToFavorites(this.props.movieId)
-                this.changeState()
-                }}>Add to Favorites</button>
-               }
-               
-               </div>
-               <h4>{this.props.movie.tagline}</h4>
-               <h2>Overview</h2>
-               <p>{this.props.movie.overview}</p>
-           </div>
-       )
-   }
+  render() {
+    return (
+      <div className="about-movie">
+        <div className="title-favorite">
+          <h2>{this.props.movie.title}</h2>
+          {this.state.isFavorite && (
+            <button className="favorite-button" disabled>
+              In Your Favorites!
+            </button>
+          )}
+          {!this.state.isFavorite && (
+            <button
+              className="favorite-button"
+              onClick={() => {
+                this.props.addToFavorites(this.props.movieId);
+                this.changeState();
+              }}
+            >
+              Add to Favorites
+            </button>
+          )}
+        </div>
+        <h4>{this.props.movie.tagline}</h4>
+        <h2>Overview</h2>
+        <p>{this.props.movie.overview}</p>
+      </div>
+    );
+  }
 }
 
-export default AboutMovie
+export default AboutMovie;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,18 +1,16 @@
-import React from 'react';
-
+import React from "react";
 
 const MovieCard = (props) => {
-    return (
-        
-        <div className="card" >
-            <h2>{props.title}</h2>
-            <div className="image">
-            <img className="poster-card" src={props.imgUrl} />
-            </div>
-        <h3>Rating: {props.rating}</h3>
-        <h4>Release Date: {props.releaseDate}</h4>
-        </div>
-    )
-}
+  return (
+    <div className="card">
+      <h2>{props.title}</h2>
+      <div className="image">
+        <img className="poster-card" src={props.imgUrl} />
+      </div>
+      <h3>Rating: {props.rating}</h3>
+      <h4>Release Date: {props.releaseDate}</h4>
+    </div>
+  );
+};
 
 export default MovieCard;
